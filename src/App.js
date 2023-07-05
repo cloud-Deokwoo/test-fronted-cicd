074a24c7b1259bd2ee9b1ee8aff3cb2301902e82
kdw335552 <kdw335552@nate.com>
2023-07-05 17:06:37 +0900

    test-cicd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          수정된 내용으로 작업 됩니다.Edit <code>src/App.js</code> and save to reload.
+          두번재로 수정한 내용입니다. 수정된 내용으로 작업 됩니다.Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
